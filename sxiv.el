cf4aa28c2992f10482a186dc3408a6d004d43776
contrapunctus <music.kashish@gmail.com>
2020-03-25 11:40:46 +0530

    Add license information as docstrings

--- sxiv.el
+++ sxiv.el
@@ -7,6 +7,15 @@
 ;; Package-Requires: ((dash "2.16.0") (emacs "25.1"))
 ;; Version: 0.3.0
 
+;; This is free and unencumbered software released into the public domain.
+;;
+;; Anyone is free to copy, modify, publish, use, compile, sell, or
+;; distribute this software, either in source code form or as a compiled
+;; binary, for any purpose, commercial or non-commercial, and by any
+;; means.
+;;
+;; For more information, please refer to <https://unlicense.org>
+
 ;;; Commentary:
 ;;
 
